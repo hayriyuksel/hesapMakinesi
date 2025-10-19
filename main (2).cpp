1daed16d6a6a94b5ac2cd0ee05b35d24af11e59a
Hayri Yüksel <hatice_c89@hotmail.com>
2025-10-19 16:12:40 +0300

    hesapMakinesi

---
+++ main (2).cpp
@@ -0,0 +1,62 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int toplama (int sayi1,int sayi2) {
+    return sayi1+sayi2;
+}
+int cikarma (int sayi1,int sayi2) {
+    return sayi1-sayi2;   
+}    
+int carpma (int sayi1,int sayi2) {
+    return sayi1*sayi2;   
+}    
+int bolme (int sayi1,int sayi2) {
+    return sayi1/sayi2;   
+}        
+    
+
+int main()
+{
+    string islemler[4]={"Topla","Cikar","Carp","Bol"};
+    cout<<"Lutfen yapmak istediginiz islemi secin"<<endl;
+    cout<<"islemler:"<<endl;
+    for(int i=0; i<4;i++) {
+        cout<<islemler[i]<<endl;
+    }
+    string secilenislem;
+    cin>>secilenislem;
+    int sayi1;
+    int sayi2;
+    if(secilenislem==islemler[0]) {
+        cin>>sayi1;
+        cin>>sayi2;
+        cout<<"Sonuc="<< toplama(sayi1,sayi2)<<endl;
+    }
+    else if(secilenislem==islemler[1]) {
+        cin>>sayi1;
+        cin>>sayi2;
+        cout<<"Sonuc="<< cikarma(sayi1,sayi2)<<endl;
+    }
+    (secilenislem==islemler[2]); {
+        cin>>sayi1;
+        cin>>sayi2;
+        cout<<"Sonuc="<< bolme(sayi1,sayi2)<<endl;
+    }
+    (secilenislem==islemler[3]); {
+        cin>>sayi1;
+        cin>>sayi2;
+        cout<<"Sonuc="<< carpma(sayi1,sayi2)<<endl;
+    }
+    
+
+    return 0;
+}
