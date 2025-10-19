5697efd95b89ab28b52cd45e96d0c35d93cdc974
Hayri Yüksel <hatice_c89@hotmail.com>
2025-10-19 16:15:53 +0300

    Update main (2).cpp

--- main (2).cpp
+++ main (2).cpp
@@ -41,17 +41,22 @@ int main()
         cin>>sayi2;
         cout<<"Sonuc="<< toplama(sayi1,sayi2)<<endl;
     }
+   if(secilenislem==islemler[0]) {
+        cin>>sayi1;
+        cin>>sayi2;
+        cout<<"Sonuc="<< toplama(sayi1,sayi2)<<endl;
+    }
     else if(secilenislem==islemler[1]) {
         cin>>sayi1;
         cin>>sayi2;
         cout<<"Sonuc="<< cikarma(sayi1,sayi2)<<endl;
     }
-    (secilenislem==islemler[2]); {
+    else if(secilenislem==islemler[2]); {
         cin>>sayi1;
         cin>>sayi2;
         cout<<"Sonuc="<< bolme(sayi1,sayi2)<<endl;
     }
-    (secilenislem==islemler[3]); {
+    if (secilenislem==islemler[3]); {
         cin>>sayi1;
         cin>>sayi2;
         cout<<"Sonuc="<< carpma(sayi1,sayi2)<<endl;
